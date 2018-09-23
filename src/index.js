fbd78af1cd9ea366eb8705a891317ed8953ea368
Daniel Huisman <daniel@huisman.me>
2018-09-23 20:24:16 +0200

    Add test code for the subtitle CLUTs

--- src/index.js
+++ src/index.js
@@ -70,8 +70,75 @@ controllerInformation.on('time-offset', (packet) => {
     console.log('[TOT]', packet.utc, 'offset:', packet.descriptors[0] ? packet.descriptors[0].parsedData[0].offset : undefined);
 });
 
+const pages = {};
+
+const yuvToRgb = (color) => {
+    const r = 1.164 * (color.y - 16) + 1.596 * (color.cr - 128);
+    const g = 1.164 * (color.y - 16) - 0.813 * (color.cr - 128) - 0.391 * (color.cb - 128);
+    const b = 1.164 * (color.y - 16) + 2.018 * (color.cb - 128);
+
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+const renderClut = (clut) => {
+    if (!clut) {
+        return;
+    }
+
+    document.getElementById(`test-clut-${clut.id}`).innerHTML = '';
+    for (const entry of Object.values(clut.entries)) {
+        const unit = document.createElement('div');
+        unit.textContent = `color ${entry.id}`;
+        unit.style.color = yuvToRgb(entry);
+
+        document.getElementById(`test-clut-${clut.id}`).appendChild(unit);
+    }
+};
+
 controllerSubtitles.on('subtitles', (packet) => {
-    console.log(packet);
+    // console.log(packet);
+
+    for (const segment of packet.segments) {
+        if (!pages[segment.pageId]) {
+            pages[segment.pageId] = {};
+        }
+        const page = pages[segment.pageId];
+
+        if (segment.type >= 16 && segment.type < 20) {
+            if (!page[segment.parsedData.versionNumber]) {
+                page[segment.parsedData.versionNumber] = {
+                    regions: {},
+                    objects: {},
+                    cluts: {}
+                };
+            }
+            const version = page[segment.parsedData.versionNumber];
+
+            if (segment.type === 16) {
+                for (const region of segment.parsedData.regions) {
+                    version.regions[region.id] = region;
+                }
+            } else if (segment.type === 17) {
+                version.regions[segment.parsedData.id] = {
+                    ...version.regions[segment.parsedData.id],
+                    ...segment.parsedData
+                };
+            } else if (segment.type === 18) {
+                version.cluts[segment.parsedData.id] = segment.parsedData;
+            } else if (segment.type === 19) {
+                version.objects[segment.parsedData.id] = segment.parsedData;
+            }
+        }
+    }
+
+    console.log(pages);
+
+    if (pages[2] && pages[2][0]) {
+        console.log('found test clut');
+
+        renderClut(pages[2][0].cluts[0]);
+        renderClut(pages[2][0].cluts[1]);
+    }
 });
 
 // // Create media source
